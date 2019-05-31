61036482bdc66d71486d20572b1f4a5eb2b0922b
kaushikee2 <48302323+kaushikee2@users.noreply.github.com>
2019-05-31 15:36:15 +0200

    Update cosine.py
    Calculate cosine distance similarity of frameworks/standards among each other.

--- cosine.py
+++ cosine.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr 22 12:39:12 2019
 
-@author: Kaushikee
-"""
 import scipy
 from scipy import spatial
 import numpy as np
@@ -11,7 +6,7 @@ import math
 import json
 from scipy.spatial.distance import cdist
 
-
+#function to calculate cosine distance 
 def cosine_similarity(vec1,vec2):
     sum11, sum12, sum22 = 0, 0, 0
     for i in range(len(vec1)):
@@ -21,17 +16,12 @@ def cosine_similarity(vec1,vec2):
         sum12 += x*y
     return sum12/math.sqrt(sum11*sum22)
 
+#loading the json file containing frameworks/standards along with their embeddings
 with open('output.json', 'r') as f:
     array = json.load(f)
 #print (array)
-'''    for i in range(len(array)):
-        for j in range(i+1,len(array)):
-            array[i] = np.array(array[i])
-            array[j] = np.array(array[j])
-            val_out = result = 1 - spatial.distance.cosine(array[i], array[j])
-print (val_out)
-'''
 
+#pass the frameworks/standards with their embeddings 
 result = {}
 for key,value in array.items():
     temp,tempDict= 0,{}
@@ -50,6 +40,4 @@ for key,value in array.items():
 
 print(result)
 
-'''val1 = min(tempDict, key=tempDict.get)
-    #res[val1] = tempDict[val1]
-    result[key]= val1     '''
+
