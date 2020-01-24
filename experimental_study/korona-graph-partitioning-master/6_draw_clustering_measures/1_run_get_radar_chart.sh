067b9943e52b1c652fa4f5f0597ca2331f1362c4
Rivas <arivasm90@gmail.com>
2020-01-24 17:45:02 +0100

    Run experiment 5-fold

--- experimental_study/korona-graph-partitioning-master/6_draw_clustering_measures/1_run_get_radar_chart.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./get_values_and_generate_radar.py ../5_clustering_measures/semEP_cm_output/ ../5_clustering_measures/metis_cm_output/ ../2_relatedness_solver/output/ output/
--- experimental_study/testing.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-from os.path import isfile, join
-from os import listdir
-
-
